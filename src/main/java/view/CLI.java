3420d257d9a035be074995776269d93ac9dd07fd
Glenn Todd <98972661+GlennT18@users.noreply.github.com>
2022-03-13 01:25:57 -0500

    Update CLI.java
    needs a few more steps in error checking and more routes for different searches

--- src/main/java/view/CLI.java
+++ src/main/java/view/CLI.java
@@ -1,5 +1,45 @@
 package view;
 
-public class CLI {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class CLI {
+    static Scanner scanner = new Scanner(System.in);
+    
+    private static List parseRequest(String request){
+        char[] characters = request.toCharArray();
+        List parsedList = new ArrayList<>();
+        String keyword = "";
+        for(char character : characters){
+            //also needs to check if character is symbol
+            if(Character.isDigit(character)){
+                return null;
+            }
+            if(character == ','){
+                parsedList.add(keyword);
+                keyword = "";
+            }else if(character == ' '){
+                continue;
+            }else{
+                keyword += character;
+            }
+        }
+        parsedList.add(keyword);
+        return parsedList;
+    }
+    public static void main(String args[]){
+        String request;
+        List command = new ArrayList<>();
+        System.out.println("Welcome to The Muze Music Library System\nPlease enter your command(s) here as a comma seperated list: ");
+        request = scanner.nextLine();
+        command = parseRequest(request);
+        while(command == null){
+            System.out.println("\n\nSorry, there was an error with you command. Please enter the data in a comma seperated list");
+            System.out.println("Ex. \"song, artist, release\" Please re-enter now:");
+            request = scanner.nextLine();
+            command = parseRequest(request);
+        }
+        System.out.println(command);
+    }
 }
