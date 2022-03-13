a076cd98ee4eff5c86684f32f2ff4821ec1bf235
Glenn Todd <98972661+GlennT18@users.noreply.github.com>
2022-03-13 14:12:02 -0400

    CLI just needs connected to the searches, user input is done.

--- src/main/java/view/CLI.java
+++ src/main/java/view/CLI.java
@@ -8,38 +8,50 @@ public class CLI {
     static Scanner scanner = new Scanner(System.in);
     
     private static List parseRequest(String request){
+        //parses string into char array
+        request = request.replaceAll("\\s", "");
         char[] characters = request.toCharArray();
         List parsedList = new ArrayList<>();
         String keyword = "";
-        for(char character : characters){
-            //also needs to check if character is symbol
-            if(Character.isDigit(character)){
+        for(char character : characters){   
+            String temp = Character.toString(character);
+            if(!(temp.matches("^[A-Za-z0-9,/]"))){
+                //checks if char is a digit or special symbol
+                System.out.println("\nDo not include symbols or digits in your command");
                 return null;
             }
             if(character == ','){
+                //if char is a comma, the word that has been compiled is put into the words list
+                //sets keyword back to blank
                 parsedList.add(keyword);
                 keyword = "";
-            }else if(character == ' '){
-                continue;
             }else{
+                //if the character is valid, it is appended to the keyword
                 keyword += character;
             }
         }
+        //adds last word and returns the list of words
         parsedList.add(keyword);
         return parsedList;
     }
     public static void main(String args[]){
+        //introduces user
         String request;
         List command = new ArrayList<>();
         System.out.println("Welcome to The Muze Music Library System\nPlease enter your command(s) here as a comma seperated list: ");
         request = scanner.nextLine();
         command = parseRequest(request);
+
+        //gathers a correct command
         while(command == null){
             System.out.println("\n\nSorry, there was an error with you command. Please enter the data in a comma seperated list");
             System.out.println("Ex. \"song, artist, release\" Please re-enter now:");
             request = scanner.nextLine();
             command = parseRequest(request);
         }
-        System.out.println(command);
+
+        //checks length of arguements, and decids what search to use upon that information
+        int arguements = command.size();
+        
     }
 }
